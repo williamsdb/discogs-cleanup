175400c93f18f5dfc224f4c0c1a266cce91e421f
Neil Thompson <github@neilthompson.co.uk>
2025-07-07 21:42:03 +0100

    Added toggle to display all entries

--- src/index.php
+++ src/index.php
@@ -123,6 +123,69 @@ try {
             padding: 10px 0;
             border-top: 1px solid lightgray;
         }
+
+        /* The switch - the box around the slider */
+        .switch {
+            position: relative;
+            display: inline-block;
+            width: 40px;
+            height: 20px;
+        }
+
+        /* Hide default HTML checkbox */
+        .switch input {
+            opacity: 0;
+            width: 0;
+            height: 0;
+        }
+
+        /* The slider */
+        .slider {
+            position: absolute;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            -webkit-transition: .4s;
+            transition: .4s;
+        }
+
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 12px;
+            width: 12px;
+            left: 4px;
+            bottom: 4px;
+            background-color: white;
+            -webkit-transition: .4s;
+            transition: .4s;
+        }
+
+        input:checked+.slider {
+            background-color: #2196F3;
+        }
+
+        input:focus+.slider {
+            box-shadow: 0 0 1px #2196F3;
+        }
+
+        input:checked+.slider:before {
+            -webkit-transform: translateX(20px);
+            -ms-transform: translateX(20px);
+            transform: translateX(20px);
+        }
+
+        /* Rounded sliders */
+        .slider.round {
+            border-radius: 20px;
+        }
+
+        .slider.round:before {
+            border-radius: 50%;
+        }
     </style>
 </head>
 
@@ -134,12 +197,36 @@ try {
     <hr style="border: none; border-top: 1px solid #d3d3d3; width: 100%; margin: 16px 0 24px 0;">
     <?php
     try {
-        $stmt = $pdo->query("SELECT id, title, image, format, type, artist, mediaGrading, sleeveGrading, url FROM release WHERE mediaGrading IS NULL OR sleeveGrading IS NULL ORDER BY title ASC ");
+        if (isset($_GET['act']) && $_GET['act'] == 'all') {
+            $stmt = $pdo->query("SELECT id, title, image, format, type, artist, mediaGrading, sleeveGrading, url FROM release ORDER BY title COLLATE NOCASE ASC");
+        } else {
+            $stmt = $pdo->query("SELECT id, title, image, format, type, artist, mediaGrading, sleeveGrading, url FROM release WHERE mediaGrading IS NULL OR sleeveGrading IS NULL ORDER BY title COLLATE NOCASE ASC");
+        }
         $releases = $stmt->fetchAll(PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
         die("Error: " . $e->getMessage());
     }
     ?>
+
+    <div style="margin-bottom: 10px;">
+        <span style="font-size: 1.1em;">Show only entries with missing gradings</span>
+        <label class="switch">
+            <input type="checkbox"
+                id="showAllToggle"
+                <?php if (!isset($_GET['act']) || (isset($_GET['act']) && $_GET['act'] === 'no')) echo 'checked'; ?>>
+            <span class="slider round"></span>
+
+        </label>
+    </div>
+    <script>
+        document.getElementById('showAllToggle').addEventListener('change', function() {
+            if (this.checked) {
+                window.location.search = '?act=no';
+            } else {
+                window.location.search = '?act=all';
+            }
+        });
+    </script>
 
     <div class="results-table">
         <table id="releases" class="display" style="width:100%">
@@ -220,6 +307,9 @@ try {
                         "orderable": false,
                         "searchable": false
                     } // Thumbnail and URL: no search
+                ],
+                "order": [
+                    [3, "asc"]
                 ]
             });
         });
